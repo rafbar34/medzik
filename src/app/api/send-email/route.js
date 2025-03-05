1aba07542db42dfab08bfe3768fd5170e538389c
rafbar34 <rafbar34@gmail.com>
2025-03-05 23:30:16 +0100

    changed dir path

--- src/app/api/send-email/route.js
+++ src/app/api/send-email/route.js
@@ -16,46 +16,44 @@ export async function POST(req) {
   if (!file) {
     return NextResponse.json({ message: 'Nie przesłano żadnego pliku.' });
   }
-  const tempDirPath = path.join(__dirname, 'uploads'); 
-  const tempFilePath = path.join(tempDirPath, file.name);
+  const tempDirPath = '/tmp/uploads'; // Zmieniamy na '/tmp', który jest dostępny na Vercel
+const tempFilePath = path.join(tempDirPath, file.name);
 
- 
-  if (!fs.existsSync(tempDirPath)) {
-    fs.mkdirSync(tempDirPath, { recursive: true }); 
-  }
+if (!fs.existsSync(tempDirPath)) {
+  fs.mkdirSync(tempDirPath, { recursive: true });
+}
 
-  const fileBuffer = Buffer.from(await file.arrayBuffer());
-  fs.writeFileSync(tempFilePath, fileBuffer);
+const fileBuffer = Buffer.from(await file.arrayBuffer());
+fs.writeFileSync(tempFilePath, fileBuffer);
 
+// Przykład wysyłania e-maila z plikiem w '/tmp'
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_TOKEN,
+  },
+});
 
-  fs.writeFileSync(tempFilePath, fileBuffer);
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      host:'stmp.gmail.com',
-      port:465,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_TOKEN, 
-      },
-    });
+const mailOptions = {
+  from: process.env.EMAIL_USER,
+  to: process.env.EMAIL_USER, // Adres docelowy
+  subject: 'Załącznik z formularza',
+  text: 'Wiadomość z załącznikiem.',
+  attachments: [
+    {
+      filename: file.name,
+      path: tempFilePath,
+    },
+  ],
+};
 
-    const mailOptions = {
-      from: process.env.EMAIL_USER,
-      to: process.env.EMAIL_USER, // Adres docelowy
-      subject: 'Załącznik z formularza',
-      text: 'Wiadomość z załącznikiem.',
-      attachments: [
-        {
-          filename: file.name, 
-          path: tempFilePath,
-        },
-      ],
-    };
-
-    try {
-      await transporter.sendMail(mailOptions);
-      return NextResponse.json({ message: 'E-mail wysłany pomyślnie' });
-    } catch (error) {
-      return NextResponse.json({ message: 'Błąd przy wysyłaniu e-maila', error: error.message }, { status: 500 });
-    }
+try {
+  await transporter.sendMail(mailOptions);
+  return NextResponse.json({ message: 'E-mail wysłany pomyślnie' });
+} catch (error) {
+  return NextResponse.json({ message: 'Błąd przy wysyłaniu e-maila', error: error.message }, { status: 500 });
+}
 };
